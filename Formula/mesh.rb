2ab3cdae7f9f6518800614ea0c7dd39a780d81bb
arianthox <arianthox@users.noreply.github.com>
2026-08-12 18:55:55 -0400

    chore: update formula SHA256 hashes for v0.2.0 / v0.1.0

--- Formula/mesh.rb
+++ Formula/mesh.rb
@@ -7,20 +7,20 @@ class Mesh < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/SteerMesh/steermesh/releases/download/mesh-v0.2.0/mesh-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "c756e0da683d878cc35bc9e4259e31f2dfd3313707126a400de74926a9f1bf1b"
     else
       url "https://github.com/SteerMesh/steermesh/releases/download/mesh-v0.2.0/mesh-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "297e5bcbbbb89a6334b9e3a8f76d70ff5b86949a9643e332dea54dbe99db0c98"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/SteerMesh/steermesh/releases/download/mesh-v0.2.0/mesh-linux-arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "297e5bcbbbb89a6334b9e3a8f76d70ff5b86949a9643e332dea54dbe99db0c98"
     else
       url "https://github.com/SteerMesh/steermesh/releases/download/mesh-v0.2.0/mesh-linux-amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "53e55b0d8305c6ef1a38c49886c4c0cd5db7aab1ac11c13fea284e3864ef0966"
     end
   end
 
--- Formula/meshnet.rb
+++ Formula/meshnet.rb
@@ -7,20 +7,20 @@ class Meshnet < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/SteerMesh/steermesh/releases/download/meshnet-v0.1.0/meshnet-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "1a94e8eb38dc21b9c69ec06a9aefdf85463e4c1c488436a306d52a2e4cd12710"
     else
       url "https://github.com/SteerMesh/steermesh/releases/download/meshnet-v0.1.0/meshnet-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "867c2c7324f36a301a44fe12aea3b4640ba8bfdd66bba60f729316c2fb09689b"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/SteerMesh/steermesh/releases/download/meshnet-v0.1.0/meshnet-linux-arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "5dbba6b9cb3dfd71ec8140452c92981168887e9257b269d5593152dbd33c748f"
     else
       url "https://github.com/SteerMesh/steermesh/releases/download/meshnet-v0.1.0/meshnet-linux-amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "867c2c7324f36a301a44fe12aea3b4640ba8bfdd66bba60f729316c2fb09689b"
     end
   end
 
